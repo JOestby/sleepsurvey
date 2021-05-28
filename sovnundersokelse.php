0ee860fc2f4d0e37ecb81c0ae446b6d3faf54d4d
Johannes Østby <johostby7@gmail.com>
2021-05-28 22:00:09 +0200

    Create sovnundersokelse.php

---
+++ sovnundersokelse.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<html lang="no">
+<head>
+    <meta charset="utf-8"/>
+	 <link rel="stylesheet" href="stil.css">
+    <title>Søvnundersøkelse</title>
+	<form method="GET" action="reg.php"</form>
+	<style>
+.center {
+  margin: auto;
+  width: 60%;
+  border: 3px solid #73AD21;
+  padding: 10px;
+}
+</style>
+</head>
+<body>
+	<div class="center">
+	<h1>Søvnundersøkelse</h1>
+	
+	<p>Skolen er bekymret for elevenes søvnvaner.</p>
+	<p2>Med denne undersøkelsen ønsker vi å få informasjon om elevenes søvnvaner.
+	Håper du vil ta deg tid til å svare på den.</p2>
+	<p3>Vi kommer til å bruke informasjonen du oppgir i en statistikk.</p>
+
+	<label for="name">Navn:</label>
+  <input type="text" id="name" name="Navn"><br><br>
+	
+	<label for="appt">Hvor mange timer sov du på Mandag?</label>
+
+<input type="number" name="Mandag"/> 
+
+<small>Velg antall timer</small>
+
+	<label for="appt">Hvor mange timer sov du på Tirsdag?</label>
+
+<input type="number" name="Tirsdag"/>
+
+<small>Velg antall timer</small>
+	
+	<label for="appt">Hvor mange timer sov du på Onsdag?</label>
+
+<input type="number" name="Onsdag"/>
+
+<small>Velg antall timer</small>
+	
+	<label for="appt">Hvor mange timer sov du på Torsdag?</label>
+
+<input type="number" name="Torsdag"
+
+<small>Velg antall timer</small>
+	
+	<label for="appt">Hvor mange timer sov du på Fredag?</label>
+
+<input type="number" name="Fredag"
+
+<small>Velg antall timer</small>
+	
+	<label for="appt">Hvor mange timer sov du på Lørdag?</label>
+
+<input type="number" name="Lørdag"
+
+	  <small>Velg antall timer</small>
+	
+	<label for="appt">Hvor mange timer sov du på Søndag?</label>
+
+<input type="number" name="Søndag"
+
+<small>Velg antall timer</small>
+
+<input type="submit" value="Send inn">
+
+</body>
+</html>
